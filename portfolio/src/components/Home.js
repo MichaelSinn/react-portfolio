7100a3e393c94cfa896d91788d4bfac030a86d70
MichaelSinn <michaelsinn.contact@gmail.com>
2022-11-17 15:56:30 -0500

    Made description longer

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -8,7 +8,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const projects = [
     {
         title: "Title",
-        description: "Description",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \n" +
+            "eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim\n" +
+            " ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\n" +
+            " aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit\n" +
+            " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur \n",
         technologies: ["React", "Node", "MongoDB", "Express"],
         src: "",
         alt: "Project hero image"
