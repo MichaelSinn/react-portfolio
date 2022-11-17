25d81df72155f4e7e0d9135fef2f090a87a91d85
MichaelSinn <michaelsinn.contact@gmail.com>
2022-11-17 15:56:03 -0500

    Styled the ProjectCard component

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -3,20 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ProjectCard({project}) {
     return (
-        <article>
-            <section>
+        <article className="container row project-card">
+            <section className="col-8">
                 <h3>{project.title}</h3>
                 <p>{project.description}</p>
-                <section>
-                    <button>Learn More</button>
-                    {project.technologies.map(tech => {
-                        return (
-                            <div>{tech}</div>
-                        );
-                    })}
+                <section className="card-footer">
+                    <button className="card-button">Learn More</button>
+                    <section className="technologies">
+                        {project.technologies.map(tech => {
+                            return (
+                                <div>{tech}</div>
+                            );
+                        })}
+                    </section>
                 </section>
             </section>
-            <section>
+            <section className="col-4 d-flex justify-content-end">
                 <img src={project.src} alt={project.alt}></img>
             </section>
         </article>
